a86aec52ac64e7bd49b8b420b88608aeef122ede
pragadeesh <pragadeesh.online@gmail.com>
2024-11-23 12:11:23 +0530

    Initial commit

---
+++ src/main/java/org/pragadeesh/spring/azure/demo/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package org.pragadeesh.spring.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Your spring boot application is deployed to azure successfully";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemoApplication.class, args);
+    }
+
+}
